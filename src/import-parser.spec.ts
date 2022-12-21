17daef6b739d58a52888d514457d2d15bf9a15d5
James Emselle <james.e.0111.2@gmail.com>
2022-12-22 07:20:09 +1100

    Finish import parsing

--- src/import-parser.spec.ts
+++ src/import-parser.spec.ts
@@ -9,8 +9,84 @@ describe("import parsing", () => {
   });
 
   it("should find no imports in empty file", () => {
-    const file = new File("", "a.ts");
+    const file = new File("a.ts", "");
     const imports = importParser.parseFile(file);
     expect(imports).toHaveLength(0);
+  });
+
+  it("should find single import in file", () => {
+    const file = new File("a.ts", "import { b } from 'b'");
+    const imports = importParser.parseFile(file);
+    expect(imports).toEqual([{ importPath: "b" }]);
+  });
+
+  it("should support trailing semicolon", () => {
+    const file = new File("a.ts", "import { b } from 'b';");
+    const imports = importParser.parseFile(file);
+    expect(imports).toEqual([{ importPath: "b" }]);
+  });
+
+  it("should support double quotes", () => {
+    const file = new File("a.ts", 'import { b } from "b";');
+    const imports = importParser.parseFile(file);
+    expect(imports).toEqual([{ importPath: "b" }]);
+  });
+
+  it("should support default export", () => {
+    const file = new File("a.ts", 'import defaultExport from "b";');
+    const imports = importParser.parseFile(file);
+    expect(imports).toEqual([{ importPath: "b" }]);
+  });
+
+  it("should support direct import", () => {
+    const file = new File("a.ts", 'import "b";');
+    const imports = importParser.parseFile(file);
+    expect(imports).toEqual([{ importPath: "b" }]);
+  });
+
+  it("should support multiple imports", () => {
+    const file = new File(
+      "a.ts",
+      `
+        import { b } from "b";
+        import { c } from "c";
+        import { d } from "d";
+      `
+    );
+    const imports = importParser.parseFile(file);
+    expect(imports.map((x) => x.importPath)).toEqual(["b", "c", "d"]);
+  });
+
+  it("should support multiple complex imports", () => {
+    const file = new File(
+      "a.ts",
+      `
+        import defaultExport from "b";
+        import * as name from "c";
+        import { export1 } from "d";
+        import { export1 as alias1 } from "e";
+        import { default as alias } from "f";
+        import { export1, export2 } from "g";
+        import { export1, export2 as alias2, /* … */ } from "h";
+        import { "string name" as alias } from "i";
+        import defaultExport, { export1, /* … */ } from "j";
+        import defaultExport, * as name from "k";
+        import "l";
+      `
+    );
+    const imports = importParser.parseFile(file);
+    expect(imports.map((x) => x.importPath)).toEqual([
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+    ]);
   });
 });
--- src/import-parser.ts
+++ src/import-parser.ts
@@ -2,6 +2,25 @@ import { File } from "./file";
 
 export class ImportParser {
   parseFile(file: File) {
-    return [];
+    const regex = /import (?:.* from ){0,1}['"](.*)['"];{0,1}/g;
+
+    let results = [];
+    let result = this.getImport(regex, file.contents);
+    while (result !== null) {
+      results.push(result);
+      result = this.getImport(regex, file.contents);
+    }
+
+    return results;
+  }
+
+  getImport(regex: RegExp, contents: string) {
+    const results = regex.exec(contents);
+
+    if (!results || results.length === 0) {
+      return null;
+    }
+
+    return { importPath: results[1] };
   }
 }
